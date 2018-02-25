9022a25e872f3338b1af0ae4ce8ff78fffe0ed6a
marlord <marlon_dx@hotmail.com>
2018-02-24 19:11:00 -0600

    Terminado Inciso 3

--- Assets/Scripts/VirtualButton.cs
+++ Assets/Scripts/VirtualButton.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class VirtualButton : MonoBehaviour {
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler {
+    private VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim; 
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        birdAnim.SetTrigger("sing");
+        Debug.Log("BUtton pushed");
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+       
+    }
+
+    // Use this for initialization
+    void Start () {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
 	
 	// Update is called once per frame
 	void Update () {
